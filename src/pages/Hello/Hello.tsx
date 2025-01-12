8c31ee65cb17a76b46ab8974f33eef26cb612eee
Huỳnh Khánh Duy <duyxanh2002@gmail.com>
2025-01-12 20:12:25 +0700

    add hello

---
+++ src/pages/Hello/Hello.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>
+        <h1>Chức năng xin chào</h1>
+        <button onClick={()=>alert('Xin chào mn')}>Xin chào</button>
+    </div>
+  )
+}
+
+export default Hello
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.scss'
 import Login from '../../components/Login'
+import Hello from '../Hello/Hello'
 
 function Home() {
   return (
     <div className='home'>
         <p>Đây là dev làm</p>
         <Login/>
+        <Hello/>
     </div>
   )
 }
