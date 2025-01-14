90f402a67810e0e4afc43a3778868001e4231018
Huỳnh Khánh Duy <duyxanh2002@gmail.com>
2025-01-14 21:56:54 +0700

    commit bằng Kraken

---
+++ src/components/UongNuoc.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UongNuoc() {
+    return (
+        <div>
+            <h1>Chức năng Uống nước
+            </h1>
+            <button onClick={() => alert('Hihi')}>Login</button>
+        </div>
+    )
+}
+
+export default UongNuoc
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,16 +3,18 @@ import './Home.scss'
 import Login from '../../components/Login'
 import Hello from '../Hello/Hello'
 import ThanhToan from '../../components/ThanhToan'
+import UongNuoc from '../../components/UongNuoc'
 
 function Home() {
   return (
     <div className='home'>
-        <p>Đây là dev làm</p>
-        
-        <Login/>
-        <Hello/>
-        <ThanhToan/>
-        <p>release đã thêm</p>
+      <p>Đây là dev làm</p>
+
+      <Login />
+      <Hello />
+      <ThanhToan />
+      <UongNuoc />
+      <p>release đã thêm</p>
     </div>
   )
 }
